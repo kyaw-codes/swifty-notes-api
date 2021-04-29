b313a34b802a97707986024a66e7e5b287d5efb3
ko kyaw <kyaw.codes@gmail.com>
2021-04-29 17:51:50 +0630

    Added LocalDateTime object mapper

--- src/main/java/com/monkey/demo/config/WebMVCConfig.java
+++ src/main/java/com/monkey/demo/config/WebMVCConfig.java
@@ -1,5 +1,9 @@
 package com.monkey.demo.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import com.monkey.demo.entity.Note;
 import com.monkey.demo.service.NoteService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,10 +15,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Configuration
-public class WebSecurityConfig {
+public class WebMVCConfig {
 
     @Autowired
     private NoteService service;
@@ -33,12 +38,25 @@ public class WebSecurityConfig {
     public CommandLineRunner runner() {
         return (args) -> {
             List.of(
-                    new Note("Note 1", LocalDateTime.now()),
-                    new Note("Note 2", LocalDateTime.now()),
-                    new Note("Note 3", LocalDateTime.now())
+                    new Note("This is Note 1", LocalDateTime.now()),
+                    new Note("This is Note 2", LocalDateTime.now()),
+                    new Note("This is Note 3", LocalDateTime.now())
             ).forEach(note -> {
                 service.createEntity(note);
             });
         };
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("EEEE, MMM d, yyyy 'at' hh:MM a");
+        SimpleModule localDateModule = new SimpleModule();
+        localDateModule.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(dateTimeFormatter));
+        localDateModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(dateTimeFormatter));
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModules(localDateModule);
+
+        return mapper;
     }
 }
